41478c49c5297cbbeee41b78217418560d4d71b4
weidenmueller-a <weidenmueller.a123@gmail.com>
2025-05-04 17:18:07 +0200

    Reduced counter display size and implemented JavaScript functionality

--- counter app js/src/js/script.js
+++ counter app js/src/js/script.js
@@ -1,2 +1,33 @@
+'use strict';
+
+const increment = document.getElementById('increment-btn');
+const decrement = document.getElementById('decrement-btn');
+const reset = document.getElementById('reset-btn');
+
+const counterValue = document.getElementById('counter-value');
+
+let myCount = 0;
+
+
+// COUNT UP
+increment.addEventListener('click', () => {
+  myCount += 1;
+  counterValue.textContent = myCount;
+});
+
+// COUNT DOWN
+decrement.addEventListener('click', () => {
+  myCount -= 1;
+  counterValue.textContent = myCount;
+});
+
+// RESET
+reset.addEventListener('click', () => {
+  myCount = 0;
+  counterValue.textContent = myCount;
+});
+
+
+
 
 
